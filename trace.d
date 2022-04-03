1b87d3686488ae668b4b5d2accf0b0a33c120482
SahithiRani <sahithirani2002@gmail.com>
2022-04-04 00:36:07 +0530

    commands added

--- trace.d
+++ trace.d
@@ -9,3 +9,47 @@ proc:::exec-success
 {
 	trace(curpsinfo->pr_psargs); 
 }
+syscall::open*:entry
+{
+	printf("%s %s",execname,copyinstr(arg0));
+}
+syscall:::entry
+{
+	@num[execname] = count();
+}
+syscall:::exit
+{
+	@num[execname] = count();
+}
+syscall:::entry
+{
+	@num[probefunc] = count();
+}
+syscall:::exit
+{
+	@num[probefunc] = count();
+}
+syscall:::entry
+{
+	@num[pid,execname] = count();
+}
+syscall:::exit
+{
+	@num[pid,execname] = count();
+}
+sysinfo:::readch
+{
+	@bytes[execname] = sum(arg0);
+}
+sysinfo:::writech
+{
+	@bytes[execname] = sum(arg0);
+}
+sysinfo:::readch
+{
+	@dist[execname] = quantize(arg0);
+}
+sysinfo:::writech
+{
+	@dist[execname] = quantize(arg0);
+}
