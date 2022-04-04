f5b80f8b1c3a7d52f07cdb9e4d495fba4df54883
Githin George <70936358+GithinGeorge2k1@users.noreply.github.com>
2022-04-04 09:18:41 +0530

    Update trace2.d

--- trace2.d
+++ trace2.d
@@ -1,16 +1,45 @@
 syscall::open*:entry
+/
+	gid == $1 || gid == $2 || gid == $3 || uid = $4
+/
 {
 	printf("%s %s",execname,copyinstr(arg0));
 }
 syscall:::entry
+/
+	gid == $1 || gid == $2 || gid == $3 || uid = $4
+/
 {
 	@num[execname] = count();
 }
 syscall:::exit
+/
+	gid == $1 || gid == $2 || gid == $3 || uid = $4
+/
 {
 	@num[probefunc] = count();
 }
 syscall:::entry
+/
+	gid == $1 || gid == $2 || gid == $3 || uid = $4
+/
 {
 	@num[pid,execname] = count();
+}
+
+proc:::start
+/
+	gid == $1 || gid == $2 || gid == $3 || uid = $4
+/
+{
+        self->start = timestamp;
+}
+
+proc:::exit
+/
+	(gid == $1 || gid == $2 || gid == $3 || uid = $4) && self->start
+/
+{
+        @[execname] = quantize(timestamp - self->start);
+        self->start = 0;
 }
