c524dc530d2866183fefcfb95ffef73d28815fd9
SahithiRani <sahithirani2002@gmail.com>
2022-04-04 19:15:30 +0530

    second file changed

--- team9_08_10_11_46_oslabd_2.d
+++ team9_08_10_11_46_oslabd_2.d
@@ -1,45 +1,39 @@
-syscall::open*:entry
+syscall::openat:entry
 /
-	gid == $1 || gid == $2 || gid == $3 || uid = $4
+	gid == $1 || gid == $2 || gid == $3 || uid == $4
 /
 {
 	printf("%s %s",execname,copyinstr(arg0));
 }
+
 syscall:::entry
 /
-	gid == $1 || gid == $2 || gid == $3 || uid = $4
+	gid == $1 || gid == $2 || gid == $3 || uid == $4
 /
 {
 	@num[execname] = count();
 }
-syscall:::exit
+
+syscall:::return
 /
-	gid == $1 || gid == $2 || gid == $3 || uid = $4
+	gid == $1 || gid == $2 || gid == $3 || uid == $4
 /
 {
 	@num[probefunc] = count();
 }
+
 syscall:::entry
 /
-	gid == $1 || gid == $2 || gid == $3 || uid = $4
+	gid == $1 || gid == $2 || gid == $3 || uid == $4
 /
 {
-	@num[pid,execname] = count();
-}
-
-proc:::start
-/
-	gid == $1 || gid == $2 || gid == $3 || uid = $4
-/
-{
-        self->start = timestamp;
+	@num[(pid,execname)] = count();
 }
 
 proc:::exit
 /
-	(gid == $1 || gid == $2 || gid == $3 || uid = $4) && self->start
+	gid == $1 || gid == $2 || gid == $3 || uid == $4
 /
 {
-        @[execname] = quantize(timestamp - self->start);
-        self->start = 0;
+        @[execname] = quantize(timestamp);
 }
