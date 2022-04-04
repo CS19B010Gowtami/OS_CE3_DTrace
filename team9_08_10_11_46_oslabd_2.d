9e7596d64fe465359e3109b5d591d376a6c8a1db
GithinGeorge <githin2k1@pop-os.localdomain>
2022-04-04 20:01:39 +0530

    changes to 2

--- team9_08_10_11_46_oslabd_2.d
+++ team9_08_10_11_46_oslabd_2.d
@@ -1,34 +1,39 @@
-syscall::openat:entry
-/
-	gid == $1 || gid == $2 || gid == $3 || uid == $4
-/
-{
-	printf("%s %s",execname,copyinstr(arg0));
-}
 
 syscall:::entry
 /
 	gid == $1 || gid == $2 || gid == $3 || uid == $4
 /
 {
+	trace(execName);
+	trace(arg0);
 	@num[execname] = count();
+	@num[(pid,execname)] = count();
 }
+
+syscall::open*:entry
+/
+	gid == $1 || gid == $2 || gid == $3 || uid == $4
+/
+{
+	/*printf("%s %s",execname,copyinstr(arg0));*/
+	trace("execName is : ");
+	trace(execName);
+	trace("Arg0 is : ");
+	trace(arg0);
+}
+
 
 syscall:::return
 /
 	gid == $1 || gid == $2 || gid == $3 || uid == $4
 /
 {
+	trace("Returning From Syscall");
+	trace(execname);
+	trace(arg0);
 	@num[probefunc] = count();
 }
 
-syscall:::entry
-/
-	gid == $1 || gid == $2 || gid == $3 || uid == $4
-/
-{
-	@num[(pid,execname)] = count();
-}
 
 proc:::exit
 /
